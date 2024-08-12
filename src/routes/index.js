39e77a0d00f4f1d636eaaf69c07d00cd63a4b075
Zobaidul Kazi <zobaidul@kazi.zobaidul>
2024-08-12 22:46:36 +0600

    🍋: mongo on run live

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/email", (req, res) => {
   res.send("Hello email world");
+});
+
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello world",
+    data: {
+      name: "Send Mailer",
+      version: "1.0.0",
+    },
+    error: {
+      code: 0,
+    },
+    status: 200,
+    timestamp: Date.now(),
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+  });
 });
 
 module.exports = router;
